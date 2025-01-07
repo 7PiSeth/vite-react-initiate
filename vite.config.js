1e42816001bc7d23755ba38116089ff004c50e83
7PiSeth <7piseth@gmail.com>
2025-01-07 16:09:27 +0700

    test config vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 3000, // This works for local development
   },
-  base: '/vite-react-initiate/', // Use your repository name
-})
+  base: '/vite-react-initiate/ending on your deployment path',
+  build: {
+    outDir: 'dist', // Output directory for build
+  },
+  publicDir: 'public', // Ensures static assets are in the "public" folder
+});
