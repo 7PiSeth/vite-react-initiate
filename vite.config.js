c59da9b752959d24a9d4bacee8b7483bc323a09b
7PiSeth <7piseth@gmail.com>
2025-01-07 16:16:22 +0700

    Revert "test config vite"
    This reverts commit 1e42816001bc7d23755ba38116089ff004c50e83.

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000, // This works for local development
+    port: 3000,
   },
-  base: '/vite-react-initiate/ending on your deployment path',
-  build: {
-    outDir: 'dist', // Output directory for build
-  },
-  publicDir: 'public', // Ensures static assets are in the "public" folder
-});
+  base: '/vite-react-initiate/', // Use your repository name
+})
